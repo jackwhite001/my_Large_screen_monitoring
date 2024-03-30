b4c581ad12b3fcdc658a528508e07f47eb3671a9
jackwhite001 <1121176166@qq.com>
2024-03-30 23:34:20 +0800

    update

--- my_echart/webpack.config.js
+++ my_echart/webpack.config.js
@@ -1,11 +1,15 @@
 const path = require('path');
 
 module.exports = {
+    lintOnSave: false,
+    // 用于打包 时 在index.html文件中引入的静态罪案路径
+    publicPath: (precess.env.NODE_ENV = 'production' ? './' : '/'),
     resolve: {
         alias: {
             '@': path.resolve(__dirname, 'src'),
         },
     },
+    // 设置sourceMap 资源映射
     devtool: 'cheap-module-source-map',
     // 模式
     mode: 'development',
